b0e783f24fa8f9999cc425b5db598c18578b71cf
Bonlver <71814443+Bonlver@users.noreply.github.com>
2020-11-17 13:34:16 +0100

    Add files via upload

---
+++ docs/ptut.php
@@ -0,0 +1,70 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE HTML>
+<html lang="fr">
+<head>
+        
+        <title>PTUT </title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="main.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <header>
+        
+        <img src="" alt="Photo de l'iut belfort montbéliard" class="rounded float left">
+        <h1 class="rounded float center">Projet Tuteuré</h1>
+        <img src="" alt="Photo du département Informatique iut belfort" class="rounded float left">
+        <nav>
+            <a href="ptut.php?partie=groupe"> Membres du groupe </a>
+            <a href="ptut.php?partie=propos">A propos</a>
+            <a href="ptut.php?partie=contacter">Nous contacter</a>
+        </nav>
+    </header>
+    <main>
+        
+        <section>
+            <h1>Projet tutoré : L'aide des logiciels de Musique assisté par ordinateur dans la conception musicale</h1>
+                <img src="zepokrc" alt="photo principale de la page d'accueil">
+            <nav>
+                <a href="decouvrir.html">Découvrir</a>
+            </nav>
+
+            <h2>Introduction</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+            
+            <img src="azeaze" alt="photo de la 1ere partie" class="rounded float left">
+            <nav>
+                <a href="partie1.html">Voir 1ère partie</a>
+            </nav>
+
+            <img src="azeaze" alt="photo de la 2eme partie" class="rounded float left">
+            <nav>
+                <a href="partie2.html">Voir 2ème partie</a>
+            </nav>
+
+            <img src="azeaze" alt="Photo de la 3eme partie" class="rounded float left">
+            <nav>
+                <a href="partie3.html">Voir 3ème partie</a>
+            </nav>
+            
+        </section>
+    
+    </main>
+
+        <footer> <! Voir Maquette conclusion bas de page>
+            
+            <nav>
+            <a href="conclusion.html">Voir Conclusion</a>
+            </nav>
+        
+        </footer> 
+
+</body>
+
+</html>
