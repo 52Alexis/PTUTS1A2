b0edf40b2205a0ee578cb642e9066cbdcfc5a31a
MoOaAaa <71814380+MoOaAaa@users.noreply.github.com>
2020-12-11 17:11:08 +0100

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,8 @@
+<div class="d-flex justify-content-around pt-5">
+    <nav>
+        <a href="partie1.html"><img  class="mr-4 rounded mb-4" src="Img/imagedepartie.png" alt="Partie 1"></a>
+        <a href="partie2.html"><img  class="mr-4 rounded mb-4" src="Img/imagedepartie.png" alt="Partie 2"></a>
+        <a href="partie3.html"><img  class="mr-4 rounded mb-4" src="Img/imagedepartie.png" alt="Partie 3"></a>
+        <a href="conclusion.html"><img  class="mr-4 rounded mb-4" src="Img/imagedepartie.png" alt="Conclusion"></a>
+    </nav>
+</div>
