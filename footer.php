9e5b0d7ac2393eb64943d6fac6348af20d25ded0
Bonlver <71814443+Bonlver@users.noreply.github.com>
2020-12-12 12:34:39 +0100

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,4 +1,21 @@
-<footer class="text-light pt-5"> 
-                <div class="text-center">
-                    <h6>Site réalisé par BERNHARD William / HEIDET Lucas / TROGNOT Mathias / VILMARD Alexis </h3>
-            </footer> 
+
+<footer class="page-footer font-small pt-4">
+    
+    <div class="container-fluid text-center text-md-left bg-dark">
+    <div class="row">
+      <div class="col-md-6 mt-md-0 mt-3">
+        <h5 class="text-uppercase font-weight-bold text-light">Membres du groupe</h5>
+        <p class="text-light">Sité réalisé par BERNHARD William, HEIDET Lucas, TROGNOT Mathias, VILMARD Alexis, dans le cadre de notre projet tutoré sur les logiciels de musique assistée par ordinateur  à l'IUT Informatique de Belfort </p>
+      </div>
+      <hr class="clearfix w-100 d-md-none pb-3">
+      <div class="col-md-6 mb-md-0 mb-3">
+        <h5 class="text-uppercase font-weight-bold text-light">Remerciements</h5>
+        <p class="text-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio deserunt fuga perferendis modi earum
+          commodi aperiam temporibus quod nulla nesciunt aliquid debitis ullam omnis quos ipsam, aspernatur id
+          excepturi hic.</p>
+    </div>
+    </div>
+  </div>
+  <div class="footer-copyright text-center py-3 text-light" style="background-color: #121214;">© 2020 LesLogicielsMao</div>
+
+</footer>
--- head.php
+++ head.php
@@ -1,26 +1,20 @@
 
 <header>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<link rel="stylesheet" href="head.css">   
 <div class="d-flex bg-dark justify-content-between">
     <div class="d-block d-inline"> 
         <img class="float-left w-25" src="DUT_info_logo_black_text.png" alt="Photo de l'iut belfort montbéliard" class="rounded float left">
-        
         <img class=" float-right ml-5 hauteur" src="IUT_logo_black_text.png" alt="Photo du département Informatique iut belfort" class="rounded float left">
     </div>    
-            <nav>
-                <div class="mt-5">
+         <nav class="navbar navbar-dark navbar-expand-sm bg-dark navbar-dark">
+            <div class="mt-5">
             <ul class="nav justify-content-end">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="index.php">Membres du groupe</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="partie1.html">A propos</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="PHPMailer/mailer.php">Nous contacter</a>
-                                </li>
-                            </ul>
-</div>
-            </nav>
+                <li class="nav-item">
+                    <a class="navbar-brand active" href="partie1.html">A propos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="navbar-brand ml-5" href="PHPMailer/mailer.php">Nous contacter</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
 </header>
