ba00c982af520b3802440c85bca279d922d4d578
MoOaAaa <lucas.heidet@orange.fr>
2020-12-15 21:53:26 +0100

    taff

--- head.php
+++ head.php
@@ -1,14 +1,14 @@
-<div class="border-dark bg-dark justify-content-between">    
+<div class="border-dark bg-dark justify-content-between">
      <nav class="navbar navbar-dark navbar-expand-sm bg-dark navbar-dark">
         <div class="mt-3">
-        <ul class="nav justify-content-start">
-            <li class="nav-item">
-                <a class="navbar-brand active pl-3 pb-3" href="partie1.php">A propos</a>
-            </li>
-            <li class="nav-item">
-                <a class="navbar-brand ml-5" href="PHPMailer/mailer.php">Nous contacter</a>
-            </li>
-        </ul>
+            <ul class="nav justify-content-start">
+                <li class="nav-item">
+                    <a class="navbar-brand active pl-3 pb-3" href="partie1.php">A propos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="navbar-brand ml-5" href="PHPMailer/mailer.php">Nous contacter</a>
+                </li>
+            </ul>
         </div>
     </nav>
 </div>
