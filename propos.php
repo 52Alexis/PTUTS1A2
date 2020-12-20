55636d5066ef1c4bc31270a549975178194e7131
Bonlver <71814443+Bonlver@users.noreply.github.com>
2020-12-20 18:24:26 +0100

    A propos / sources / remerciements

---
+++ propos.php
@@ -0,0 +1,53 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+<head>
+    <title>PTUT</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body class="bg-dark">
+    <?php include("head.php") ?>
+    <main> 
+    <div class="container-fluid">
+        <div class="row">
+
+            <div class="col bg-dark text-center text-light border-right border-bottom border-light p-5"><h2 class="text-justify">Remerciements</h2>
+            <p class="text-justify">Nous remercions nos professeurs référents, Mme Couturier, M Millet et M Sahler de nous avoir suivis et guidé tout au long de notre projet. Nous remercions également toutes les personnes ayant pris le temps de répondre à notre sondage. </p>
+            </div>
+            <div class="col bg-dark text-center text-light p-5 border-bottom border-light"><h2>Nos Sources / Sitographie</h2>
+            <ul>
+                    <li class="text-justify pt-2">Collaborateurs Wikipédia, « Musique assistée par ordinateur », Wikipédia, 25/07/2020 [dernière consultation le 24/11/2020], disponible à l’adresse: <a href="https://fr.wikipedia.org/wiki/Musique_assist%C3%A9e_par_ordinateur">https://fr.wikipedia.org/wiki/Musique_assist%C3%A9e_par_ordinateur</a>
+
+                    </li>
+
+
+                    <li class="text-justify pt-2">Collaborateurs Wikipédia, « Musique assistée par ordinateur », Wikipédia, 25/07/2020 [dernière consultation le 24/11/2020], disponible à l’adresse: <a href="https://fr.wikipedia.org/wiki/Musique_assist%C3%A9e_par_ordinateur">https://fr.wikipedia.org/wiki/Musique_assist%C3%A9e_par_ordinateur</a>
+                    </li>
+
+                    <li class="text-justify pt-2">Collaborateurs Wikipédia, « Virtual Studio Technology », Wikipédia, 26/10/2020 [dernière consultation le 24/11/2020], disponible à l’adresse: <a href="https://fr.wikipedia.org/wiki/Virtual_Studio_Technology">https://fr.wikipedia.org/wiki/Virtual_Studio_Technology</a>
+
+
+                    </li>
+
+                    <li class="text-justify pt-2">Collaborateurs Wikipédia, « Instrument virtuel », Wikipédia, 26/10/2020 [dernière consultation le 24/11/2020], disponible à l’adresse: <a href="https://fr.wikipedia.org/wiki/Instrument_virtuel">https://fr.wikipedia.org/wiki/Instrument_virtuel</a>
+
+                    </li>
+
+                    <li class="text-justify pt-2">Collaborateurs Wikipédia, « Musical Instrument Digital Interface », Wikipédia, 23/11/2020 [consultée le 24/11/2020], disponible à l’adresse : <a href="https://fr.wikipedia.org/wiki/Musical_Instrument_Digital_Interface">https://fr.wikipedia.org/wiki/Musical_Instrument_Digital_Interface</a>
+
+                    </li>
+                </ul>
+            </div>
+            <br><br><br>
+        </div>
+    </main>
+<div class="fixed-bottom">
+<?php include("footer.php"); ?>
+</div>
+
+
+</body>
+
+</html>
