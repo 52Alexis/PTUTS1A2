eabe2ed0b41ebf22ce51b39b4548b7ef2c26e8e2
52Alexis <30575465+52Alexis@users.noreply.github.com>
2020-12-20 16:22:29 +0100

    imgs

--- partie1.php
+++ partie1.php
@@ -6,8 +6,8 @@
     <link rel="stylesheet" href="index.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body class="bg-dark">
     
@@ -18,7 +18,7 @@
     <div class="container-fluid">
         <div class="row">
                 
-            <div class="col bg-dark text-center text-light border-right border-light"><h4 class="text-justify">Dans la catégorie des logiciels de MAO, il existe 6 types de logiciels différents. Nous allons les lister ici :</h1>
+            <div class="col bg-dark text-center text-light border-right border-light"><h4 class="text-justify">Dans la catégorie des logiciels de MAO, il existe 6 types de logiciels différents. Nous allons les lister ici :</h4>
             <ul>
                 <li class="text-justify pt-2">Les séquenceurs : les logiciels les plus utilisés pour composer de la musique sur ordinateur. Ils permettent une énorme palette de possibilité de sons, instruments virtuels et effets modulant les samples et instruments à loisir. De plus, il est possible d’acheter et d’installer des instruments et des packs de samples supplémentaires pour augmenter encore plus la banque dans laquelle vous piocher pour créer votre musique.
                 </li>
