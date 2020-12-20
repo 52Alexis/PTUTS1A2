730975cd87331bc85896cc174357cd8e04fe0d88
MoOaAaa <lucas.heidet@orange.fr>
2020-12-20 21:43:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,41 +13,41 @@
             <section>
                 <div class="box">
                     <img src="img/head.jpg" class="img-fluid" alt="Image accueil">
-                            
+
                     <nav>
                         <a href="decouvrir.php" class="legende2 text-light"><h6 class="text-light">Découvrir : L'aide des logiciels de Musique assistée par ordinateur dans la conception musicale</h6></a>
                     </nav>
                 </div>
             </section>
-                    
+
             <section class="text-light">
                 <div class="ml-3">
                     <h2>Introduction</h2>
                 </div>
                 <div class="ml-5">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                </div>   
-                
+                </div>
+
                     <br>
                     <div class="container-fluid">
                     <div class="row">
-                
-                        <div class="col bg-dark text-center text-light  rounded-left"><a href="partie1.php"><img src="imagedepartie.png" class="img-responsive img-thumbnail"></a></div>
-                
-                        <div class="d-none d-sm-block col bg-dark text-center text-light"><a href="partie2.php"><img src="imagedepartie.png" class="img-responsive img-thumbnail"></a></div>
-                
-                        <div class="d-md-none d-lg-block d-none d-sm-block col bg-dark text-center text-light rounded-right mr-5"><a href="partie3.php"><img src="imagedepartie.png" class="img-responsive img-thumbnail"></a></div>
-            
+
+                        <div class="col bg-dark text-center text-light  rounded-left"><a href="partie1.php"><img src="img/Ableton.png" class="img-responsive img-thumbnail"></a></div>
+
+                        <div class="d-none d-sm-block col bg-dark text-center text-light"><a href="partie2.php"><img src="img/partie2.png" class="img-responsive img-thumbnail"></a></div>
+
+                        <div class="d-md-none d-lg-block d-none d-sm-block col bg-dark text-center text-light rounded-right mr-5"><a href="partie3.php"><img src="img/graphe1.png" class="img-responsive img-thumbnail"></a></div>
+
                     </div>
                     </div>
-                
+
                 <nav>
                     <div>
                         <br>
                         <div class="text-center pt-5">
                             <a href="conclusion.php"  class="btn btn-outline-primary btn-lg">Voir conclusion</a>
                         </div>
-                    </div>  
+                    </div>
                 </nav>
             </section>
             <br>
